f53f31f6b541ad66378d5af887f00417ecfb3511
GBFerro <giovani.ferro@usp.br>
2023-04-24 13:42:30 -0300

    Add project files.

---
+++ projMongoDbAPI/Controllers/ClientController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using projMongoDbAPI.Models;
+using projMongoDbAPI.Services;
+
+namespace projMongoDbAPI.Controllers
+{
+    // Define o prefixo para o roteamento de requisições, que será "api/Client"
+    [Route("api/[controller]")]
+    // Define que esta classe é um controlador de API
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        // Declara uma instância da classe ClientService, que será responsável por realizar
+        // as operações no banco de dados MongoDB relacionadas ao modelo "Client".
+        private readonly ClientService _clientService;
+
+        // Define o construtor da classe ClientController, que recebe uma instância da classe
+        // ClientService como parâmetro e a injeta no objeto _clientService.
+        public ClientController(ClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        // Define o endpoint HTTP GET para obter todos os objetos "Client" do banco de dados.
+        // Retorna uma lista de objetos Client encapsulada em um objeto ActionResult.
+        [HttpGet]
+        public ActionResult<List<Client>> Get() => _clientService.Get();
+
+        // Define o endpoint HTTP GET para obter um objeto "Client" específico do banco de dados, definido pelo parâmetro id.
+        // Retorna um objeto Client encapsulado em um objeto ActionResult. Se o objeto não for encontrado, retorna NotFound().
+        [HttpGet("{id:length(24)}", Name = "GetClient")]
+        public ActionResult<Client> Get(string id)
+        {
+            var client = _clientService.Get(id);
+            if (client == null) return NotFound();
+
+            return client;
+        }
+
+        // Define o endpoint HTTP POST para criar um novo objeto "Client" no banco de dados.
+        // Recebe um objeto Client como parâmetro e retorna o mesmo objeto encapsulado em um objeto ActionResult.
+        [HttpPost]
+        public ActionResult<Client> Create(Client client) => _clientService.Create(client);
+
+        // Define o endpoint HTTP PUT para atualizar um objeto "Client" específico no banco de dados, definido pelo parâmetro id.
+        // Recebe um objeto Client como parâmetro e retorna Ok() se a atualização for bem-sucedida.
+        // Se o objeto não for encontrado, retorna NotFound().
+        [HttpPut("{id:length(24)}", Name = "PutClient")]
+        public ActionResult Update(string id, Client client)
+        {
+            var c = _clientService.Get(id);
+            if (c == null) return NotFound();
+
+            _clientService.Update(id, client);
+
+            return Ok();
+        }
+
+        // Define o endpoint HTTP DELETE para remover um objeto "Client" específico do banco de dados, definido pelo parâmetro id.
+        // Retorna Ok() se a remoção for bem-sucedida. Se o objeto não for encontrado, retorna NotFound().
+        [HttpDelete("{id:length(24)}", Name = "DeleteClient")]
+        public ActionResult Delete(string id)
+        {
+            if (id == null) return NotFound();
+
+            var client = _clientService.Get(id);
+            if (client == null) return NotFound();
+
+            _clientService.Delete(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ projMongoDbAPI/Services/ClientService.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using MongoDB.Driver;
+using projMongoDbAPI.Config;
+using projMongoDbAPI.Models;
+
+namespace projMongoDbAPI.Services
+{
+    public class ClientService
+    {
+        private readonly IMongoCollection<Client> _client;
+
+        // Construtor que configura a coleção de clientes do MongoDB
+        public ClientService(IProjMDSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _client = database.GetCollection<Client>(settings.ClientCollectionName);
+        }
+
+        // Obtém todos os clientes da coleção de clientes do MongoDB
+        public List<Client> Get() => _client.Find(c => true).ToList();
+
+        // Obtém um cliente específico pelo ID da coleção de clientes do MongoDB
+        public Client Get(string id) => _client.Find(c => c.Id == id).FirstOrDefault();
+
+        // Cria um novo cliente na coleção de clientes do MongoDB
+        public Client Create(Client client)
+        {
+            _client.InsertOne(client);
+            return client;
+        }
+
+        // Atualiza um cliente na coleção de clientes do MongoDB usando o objeto do cliente
+        public void Update(Client client) => _client.ReplaceOne(c => c.Id == client.Id, client);
+
+        // Atualiza um cliente na coleção de clientes do MongoDB usando o ID do cliente e o objeto do cliente atualizado
+        public void Update(string id, Client client) => _client.ReplaceOne(c => c.Id == id, client);
+
+        // Deleta um cliente da coleção de clientes do MongoDB pelo ID do cliente
+        public void Delete(string id) => _client.DeleteOne(c => c.Id == id);
+
+        // Deleta um cliente da coleção de clientes do MongoDB usando o objeto do cliente
+
+        public void Delete (Client client) => _client.DeleteOne(c => c.Id == client.Id);
+    }
+}
